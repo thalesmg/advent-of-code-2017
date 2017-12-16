b081a5b48c2ad164396b302260b707db4c76a630
Thales Macedo Garitezi <thalesmg@gmail.com>
2017-12-16 11:37:40 -0200

    Dia 10 parte 2.

--- day10/Day10.rs
+++ day10/Day10.rs
@@ -3,7 +3,7 @@ use std::collections::VecDeque;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn twist(ribbon: &mut HashMap<usize, usize>, cur_index: usize, length: usize) {
+fn twist(ribbon: &mut HashMap<usize, usize>, cur_index: usize, length: &usize) {
     let mut indices = gen_indices(&ribbon, cur_index, length);
 
     while indices.len() > 0 {
@@ -28,7 +28,7 @@ fn twist(ribbon: &mut HashMap<usize, usize>, cur_index: usize, length: usize) {
     }
 }
 
-fn gen_indices(ribbon: &HashMap<usize, usize>, cur_index: usize, length: usize) -> VecDeque<usize> {
+fn gen_indices(ribbon: &HashMap<usize, usize>, cur_index: usize, length: &usize) -> VecDeque<usize> {
     let size = ribbon.len();
     let mut indices: VecDeque<usize> = VecDeque::new();
     for i in cur_index..(cur_index + length) {
@@ -46,8 +46,8 @@ fn test() {
         ribbon.insert(i, i);
     }
     let size = ribbon.len();
-    for len in lens {
-        twist(&mut ribbon, cur_index, len);
+    for len in &lens {
+        twist(&mut ribbon, cur_index, &len);
         cur_index = (cur_index + len + skip_size) % size;
         skip_size += 1;
     }
@@ -72,7 +72,7 @@ fn part1() {
     }
     let size = ribbon.len();
     for len in lens {
-        twist(&mut ribbon, cur_index, len);
+        twist(&mut ribbon, cur_index, &len);
         cur_index = (cur_index + len + skip_size) % size;
         skip_size += 1;
     }
@@ -80,6 +80,60 @@ fn part1() {
     println!("{}", ribbon[&(0 as usize)] * ribbon[&(1 as usize)]);
 }
 
+fn read_contents() -> String {
+    let mut f = File::open("input.txt").expect("bum!");
+    let mut contents: String = String::new();
+    f.read_to_string(&mut contents)
+        .expect("algo cagou!");
+    contents
+}
+
+fn part2(contents: String) {
+    let mut lens: Vec<usize> = vec![17, 31, 73, 47, 23];
+    let mut input = contents
+        .trim()
+        .chars()
+        .map(|chr| {
+            chr as usize
+        })
+        .collect::<Vec<usize>>();
+    input.append(&mut lens);
+
+    println!("lens: {:?}", lens);
+    println!("input: {:?}", input);
+
+    let mut ribbon = HashMap::new();
+    let mut cur_index = 0;
+    let mut skip_size = 0;
+    for i in 0..256 {
+        ribbon.insert(i, i);
+    }
+    let size = ribbon.len();
+
+    for _round in 0..64 {
+        for len in &input {
+            twist(&mut ribbon, cur_index, &len);
+            cur_index = (cur_index + len + skip_size) % size;
+            skip_size += 1;
+        }
+    }
+
+    let mut output: Vec<usize> = vec![];
+    for i in 0..16 {
+        let mut digit = 0;
+        for j in (i * 16)..(i * 16 + 16) {
+            digit ^= ribbon[&j];
+        }
+        output.push(digit);
+    }
+    for digit in output {
+        print!("{:x}", digit);
+    }
+    println!("");
+}
+
 fn main() {
     part1();
+    // part2(String::from(""));
+    part2(read_contents());
 }
