d0c6a8428ff6dc23ab638839f07bb131d90b92e7
Thales Macedo Garitezi <thalesmg@gmail.com>
2017-12-30 19:20:39 -0200

    Dia 17 parte 1.

---
+++ day17/Dia17.hs
@@ -0,0 +1,62 @@
+module Main where
+
+import qualified Data.Sequence as S
+import Data.Sequence (Seq)
+
+type Buffer = (Int, Int, Seq Int)
+
+spin :: Int -> Buffer -> Buffer
+spin steps (size, pos, buf) =
+  let
+    nextIndex = 1 + mod (pos + steps) size
+  in
+    (succ size, nextIndex, S.insertAt nextIndex size buf)
+
+initial :: Buffer
+initial = (1, 0, S.singleton 0)
+
+inputSteps :: Int
+inputSteps = 363
+
+solution1 :: Maybe Int
+solution1 =
+  let
+    (_, _, final) = iterate (spin inputSteps) initial !! 2017
+  in
+    S.findIndexR (== 2017) final
+
+solution2 :: Int
+solution2 = go 50000000 (1, 0, 0)
+  where
+    go :: Int -> (Int, Int, Int) -> Int
+    go 0 (_, _, acc) = acc
+    go n (size, pos, acc) =
+      let
+        nextIndex = 1 + mod (pos + inputSteps) size
+        state' = (succ size, nextIndex, if nextIndex == 1 then size else acc)
+      in
+        seq state' (go (pred n) state')
+
+-- import qualified Data.IntMap as M
+-- import Data.IntMap (IntMap, (!))
+
+-- type Buffer = (Int, IntMap Int)
+
+-- insertMany :: [(Int, v)] -> IntMap v -> IntMap v
+-- insertMany lst m = foldr (\(k, v) acc -> M.insert k v acc) m lst
+
+-- spin :: Int -> Buffer -> Buffer
+-- spin steps (size, buffer) =
+--   let
+--     nextPos = mod (size + steps) size
+--     lastPred = buffer ! nextPos
+--   in
+--     (succ size, insertMany [(nextPos, size), (size, lastPred)] buffer)
+
+-- initial :: Buffer
+-- initial = (1, M.singleton 0 0)
+
+-- spin' = spin 3
+
+main :: IO ()
+main = print solution2
