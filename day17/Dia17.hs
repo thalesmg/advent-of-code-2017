b434f0ba30f62caaf841ed96f561819162173815
Thales Macedo Garitezi <tgaritezi@xerpa.com.br>
2018-01-03 11:22:17 -0200

    Dia 17 parte 2 sem problemas de memória!!!!!!

--- day17/Dia17.hs
+++ day17/Dia17.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
 import qualified Data.Sequence as S
@@ -30,33 +32,12 @@ solution2 n = go n (1, 0, 0)
   where
     go :: Int -> (Int, Int, Int) -> Int
     go 0 (_, _, acc) = acc
-    go n (size, pos, acc) =
+    go n (size, pos, !acc) =
       let
         nextIndex = 1 + mod (pos + inputSteps) size
-        state' = (size + 1, seq nextIndex nextIndex, if nextIndex == 1 then size else acc)
+        state' = (size + 1, nextIndex, if nextIndex == 1 then size else acc)
       in
-        seq state' (go (n - 1) state')
-
--- import qualified Data.IntMap as M
--- import Data.IntMap (IntMap, (!))
-
--- type Buffer = (Int, IntMap Int)
-
--- insertMany :: [(Int, v)] -> IntMap v -> IntMap v
--- insertMany lst m = foldr (\(k, v) acc -> M.insert k v acc) m lst
-
--- spin :: Int -> Buffer -> Buffer
--- spin steps (size, buffer) =
---   let
---     nextPos = mod (size + steps) size
---     lastPred = buffer ! nextPos
---   in
---     (succ size, insertMany [(nextPos, size), (size, lastPred)] buffer)
-
--- initial :: Buffer
--- initial = (1, M.singleton 0 0)
-
--- spin' = spin 3
+        go (n - 1) state'
 
 main :: IO ()
 main = print $ solution2 50000000
